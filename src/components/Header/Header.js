e70c215790b655c732b753a4d25696bd4b0c7762
Claire <trucy.claire92@gmail.com>
2023-02-20 21:47:04 +0100

    Burger menu + animation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,65 @@
 import { NavLink } from 'react-router-dom';
 import './styles.scss';
+import { useState } from 'react';
 import logo from '../../assets/img/logo/Logo.png';
 
 export default function Header() {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleShowLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
   return (
-    <div className="header">
+    <div className={`header ${showLinks ? 'show-nav' : 'hide-nav'}`}>
 
       <img alt="website logo" className="header_logo" src={logo} />
 
       <div className="header_nav">
-        <NavLink
-          to="/"
-          className={(isActive) => (isActive ? 'header_nav_link header_nav_link--active' : 'header_nav_link')}
-        >
-          Accueil
-        </NavLink>
-        <NavLink
-          to="/poster-une-annonce"
-          className={(isActive) => (isActive ? 'header_nav_link header_nav_link--active' : 'header_nav_link')}
-        >
-          Poster une annonce
-        </NavLink>
-        <NavLink
-          to="/mon-profil"
-          className={(isActive) => (isActive ? 'header_nav_link header_nav_link--active' : 'header_nav_link')}
-        >
-          Mon profil
-        </NavLink>
-        <NavLink
-          to="/connexion"
-          className="header_button"
-        >
-          Connexion
-        </NavLink>
-        <NavLink
-          to="/inscription"
-          className="header_button"
-        >
-          Inscription
-        </NavLink>
+        <li className="header_nav_item slideInDown-1">
+          <NavLink
+            to="/"
+            className={(isActive) => (isActive ? 'header_nav_link header_nav_link--active' : 'header_nav_link')}
+          >
+            Accueil
+          </NavLink>
+        </li>
+        <li className="header_nav_item slideInDown-2">
+          <NavLink
+            to="/poster-une-annonce"
+            className={(isActive) => (isActive ? 'header_nav_link header_nav_link--active' : 'header_nav_link')}
+          >
+            Poster une annonce
+          </NavLink>
+        </li>
+        <li className="header_nav_item slideInDown-3">
+          <NavLink
+            to="/mon-profil"
+            className={(isActive) => (isActive ? 'header_nav_link header_nav_link--active' : 'header_nav_link')}
+          >
+            Mon profil
+          </NavLink>
+        </li>
+        <li className="header_nav_item slideInDown-4">
+          <NavLink
+            to="/connexion"
+            className={(isActive) => (isActive ? 'header_nav_link header_nav_link--active' : 'header_nav_link')}
+          >
+            Connexion
+          </NavLink>
+        </li>
+        <li className="header_nav_item slideInDown-5">
+          <NavLink
+            to="/inscription"
+            className={(isActive) => (isActive ? 'header_nav_link header_nav_link--active' : 'header_nav_link')}
+          >
+            Inscription
+          </NavLink>
+        </li>
       </div>
-
+      <button type="button" className="header_burger">
+        <span className="burger_bar" onClick={handleShowLinks} />
+      </button>
     </div>
   );
 }
