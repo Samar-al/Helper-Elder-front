bd916201a812575f315c11fc0c845ee42b46311b
Cboudaud <camilleboudaud@gmail.com>
2023-02-21 15:23:04 +0100

    data file created + logo bug fixed

--- src/components/ServiceCards/Card.js
+++ src/components/ServiceCards/Card.js
@@ -1,15 +1,18 @@
 import './styles.scss';
-import cardPlaceholder from '../../assets/img/card_placeholder.png';
+import PropTypes from 'prop-types';
 
-export default function Card() {
-  
+export default function Card({ name, logo }) {
   return (
           <div className="card">
-            <img alt="website logo" className="card_logo" src={cardPlaceholder} />
-            <h1 className="card_title">Courses</h1>
+            <img alt="website logo" className="card_logo" src={logo} />
+            <h1 className="card_title">{name}</h1>
           </div>
   );
 }
 
 
  
+Card.propTypes = {
+  logo: PropTypes.string.isRequired, // à modifier lorsqu'on saura comment récupérer l'avatar
+  name: PropTypes.string.isRequired,
+};
--- src/components/ServiceCards/ServiceCards.js
+++ src/components/ServiceCards/ServiceCards.js
@@ -1,17 +1,28 @@
 import './styles.scss';
 import Card from './Card';
+import cards from './data'; // data temporaires
+import PropTypes from 'prop-types';
 
 export default function ServiceCards() {
   return (
     <div className="servicecards">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  );
-}
+      {cards.map ((card) => (
+      <Card 
+      key={card.id}
+      {...card} 
+      />
+      ))}
+      </div>
+    );
+  }
+  
+// ServiceCards.propTypes = {
+//     cards: PropTypes.arrayOf(PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       logo: PropTypes.string.isRequired, // à modifier lorsqu'on saura comment récupérer l'avatar
+//       name: PropTypes.string.isRequired,
+//       description: PropTypes.string.isRequired,
+//       date: PropTypes.instanceOf(Date).isRequired,
+//     })).isRequired,
+//   };
+  
