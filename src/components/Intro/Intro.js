2689dc54a8ecec31a455cb800b34c406660a0c3e
Claire <trucy.claire92@gmail.com>
2023-02-21 17:40:03 +0100

    header version test finished

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -5,8 +5,10 @@ export default function Intro() {
   return (
     <div className="intro">
       <img src={background} alt="background" />
-      <h1 className="intro_title">HELPERS-ELDERS</h1>
-      <p className="intro_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <div className="content">
+        <h1 className="intro_title">HELPERS-ELDERS</h1>
+        <p className="intro_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
     </div>
   );
 }
