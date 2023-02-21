7ef3882361a68a2d39c75def6bc6b934791244ae
Claire <trucy.claire92@gmail.com>
2023-02-21 17:17:51 +0100

    background removed from app, and placed in intro + start css

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,8 +1,10 @@
 import './styles.scss';
+import background from '../../../public/background_intro.png';
 
 export default function Intro() {
   return (
     <div className="intro">
+      <img src={background} alt="background" />
       <h1 className="intro_title">HELPERS-ELDERS</h1>
       <p className="intro_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
