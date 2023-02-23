0119176593bdedf5b31be671b412969b3ef69c34
Cboudaud <camilleboudaud@gmail.com>
2023-02-23 10:08:13 +0100

    Connexion design done

--- src/components/Connexion/Connexion.js
+++ src/components/Connexion/Connexion.js
@@ -16,17 +16,26 @@ export default function Connexion() {
               <div className="connexion_title"> Connexion </div>
                 <form className="connexion_form">
                   <div className="connexion_form_item">
-                    <FormControl fullWidth className="connexion_form_item_type" size="small">
-                    <TextField label="E-mail" size="small" value={emailInput} onChange={(e) => dispatch(typeEmail(e.target.value))} />
-                    <TextField label="Mot de passe" size="small" value={passwordInput} onChange={(e) => dispatch(typePassword(e.target.value))} />
+                    <FormControl fullWidth size="small">
+                      <TextField 
+                          label="E-mail" size="small" 
+                          value={emailInput} 
+                          onChange={(e) => dispatch(typeEmail(e.target.value))} 
+                      />
                     </FormControl>
-                  </div> 
-                  <button
-                    className="connexion_button"
-                    type="submit"
-                    > Se connecter
-                  </button>
-
+                  </div>
+                  <div className="connexion_form_item">
+                    <FormControl fullWidth size="small">
+                      <TextField 
+                        label="Mot de passe" 
+                        size="small" 
+                        value={passwordInput} onChange={(e) => dispatch(typePassword(e.target.value))} 
+                      />
+                    </FormControl>
+                  </div>
+                  <div className="connexion_button">
+                    <Button variant="contained">Se connecter</Button>
+                  </div>
                 </form>
           </div>
   );
