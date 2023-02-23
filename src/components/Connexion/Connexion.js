93558cac36fbeade498cb4b835e8206d00d62a1d
Cboudaud <camilleboudaud@gmail.com>
2023-02-23 16:36:02 +0100

    identation modified in Connexion component

--- src/components/Connexion/Connexion.js
+++ src/components/Connexion/Connexion.js
@@ -17,36 +17,35 @@ export default function Connexion() {
     dispatch(handleLogin());
   };
 
-
   return (
-            <div className="connexion">
-                <div className="connexion_title"> Connexion </div>
-                  <form className="connexion_form" onSubmit={handleSubmit}>
-                    <div className="connexion_form_item">
-                      <FormControl fullWidth size="small">
-                        <TextField 
-                            label="E-mail" size="small" 
-                            value={emailInput} 
-                            onChange={(e) => dispatch(typeEmail(e.target.value))} 
-                        />
-                      </FormControl>
-                    </div>
-                    <div className="connexion_form_item">
-                      <FormControl fullWidth size="small">
-                        <TextField 
-                          label="Mot de passe" 
-                          size="small" 
-                          value={passwordInput} onChange={(e) => dispatch(typePassword(e.target.value))} 
-                        />
-                      </FormControl>
-                    </div>
-                    <div className="connexion_login_button">
-                      <Button variant="contained">Se connecter</Button>
-                    </div>
-                  </form>
+    <div className="connexion">
+        <div className="connexion_title"> Connexion </div>
+          <form className="connexion_form" onSubmit={handleSubmit}>
+            <div className="connexion_form_item">
+              <FormControl fullWidth size="small">
+                <TextField 
+                    label="E-mail" 
+                    size="small" 
+                    value={emailInput} 
+                    onChange={(e) => dispatch(typeEmail(e.target.value))} 
+                />
+              </FormControl>
             </div>
-         )
-
-
+            <div className="connexion_form_item">
+              <FormControl fullWidth size="small">
+                <TextField 
+                  label="Mot de passe" 
+                  size="small" 
+                  value={passwordInput} 
+                  onChange={(e) => dispatch(typePassword(e.target.value))} 
+                />
+              </FormControl>
+            </div>
+            <div className="connexion_login_button">
+              <Button variant="contained">Se connecter</Button>
+            </div>
+          </form>
+      </div>
+  )
 };
 
