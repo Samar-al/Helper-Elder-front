02ee6f73e73eee73a754ff3ee080cdc474dc6645
Cboudaud <camilleboudaud@gmail.com>
2023-02-23 10:58:23 +0100

    Authentification actions + reducer created

---
+++ src/actions/authentification.js
@@ -0,0 +1,18 @@
+// actions creators
+export const HANDLE_LOGIN = 'HANDLE_LOGIN'; // For the middleware (later)
+export const HANDLE_LOGOUT = 'HANDLE_LOGOUT';
+export const SAVE_SUCCESSFUL_AUTH = 'SAVE_SUCCESSFUL_AUTH';
+
+// actions types
+export const handleLogin = () => ({
+  type: HANDLE_LOGIN,
+});
+
+export const handleLogout = () => ({
+  type: HANDLE_LOGOUT,
+});
+
+export const saveSuccessfulAuth = (loggedMessage) => ({
+  type: SAVE_SUCCESSFUL_AUTH,
+  loggedMessage: loggedMessage,
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import searchbarReducer from './searchbar';
 import burgerReducer from './burger';
 import connexionReducer from './connexion';
+import authentificationReducer from './authentification';
 
 const rootReducer = combineReducers({
   searchbar: searchbarReducer,
   burger: burgerReducer,
   connexion: connexionReducer,
+  authentification : authentificationReducer,
 });
 
 export default rootReducer;
