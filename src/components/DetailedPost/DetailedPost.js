ee7c2a9b89d863569cb773569cb94bfa6c316b1d
Claire <trucy.claire92@gmail.com>
2023-02-24 17:32:36 +0100

    date added + jsx setup

--- src/components/DetailedPost/DetailedPost.js
+++ src/components/DetailedPost/DetailedPost.js
@@ -1,10 +1,34 @@
+import { Button, Rating, Typography } from '@mui/material';
 import './styles.scss';
+import PropTypes from 'prop-types';
 
-export default function DetailedPost() {
+export default function DetailedPost({ user }) {
+  console.log(user);
   return (
     <div className="main">
       <div className="detailed-post">
-
+        <div className="detailed-post_left">
+          <img src="#" alt="profil" />
+          <p className="detailed-post_left_user">José</p>
+          <Typography component="legend" />
+          <Rating name="note" value={3} readOnly />
+          <ul className="detailed-post_left_service">
+            Services proposés:
+            <li className="detailed-post_left_service_item">Compagnie</li>
+            <li className="detailed-post_left_service_item">Courses</li>
+            <li className="detailed-post_left_service_item">Chauffeur</li>
+            <li className="detailed-post_left_service_item">Cuisine</li>
+            <li className="detailed-post_left_service_item">Ménage</li>
+          </ul>
+          <p>Tarifs 12€ de l'heure</p>
+        </div>
+        <div className="detailed-post_right">
+          <div className="detailed-post_right_header">
+            <h1 className="detailed-post_right_title">Titre de l'annonce <span className="detailed-post_right_subtitle">Service Ponctuel</span></h1>
+            <Button type="submit" variant="contained">Envoyer un message</Button>
+          </div>
+          <p className="detailed-post_right_content">Blablabla</p>
+        </div>
       </div>
       <div className="view">
         <h2 className="view_title">Les avis:</h2>
@@ -16,3 +40,17 @@ export default function DetailedPost() {
     </div>
   );
 }
+
+DetailedPost.propTypes = {
+  user: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      username: PropTypes.string.isRequired,
+      picture: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      zipcode: PropTypes.string.isRequired,
+      city: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
--- src/components/DetailedPost/data.js
+++ src/components/DetailedPost/data.js
@@ -7,7 +7,6 @@ export default [
     zipcode: '34090',
     city: 'Montpellier',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non est sit amet felis finibus maximus. Proin vel fermentum mauris, a faucibus risus. Duis at urna nec turpis luctus varius ac non est. Aliquam a est dapibus, cursus ex et, egestas justo. Curabitur vel magna mollis, pharetra arcu non, commodo neque. Mauris sapien sem, facilisis quis nunc et, lacinia maximus massa. Duis eros elit, egestas ac libero at, eleifend mollis mi.',
-    date: Date.now(),
     view: [
       {
         username: 'Thierry',
