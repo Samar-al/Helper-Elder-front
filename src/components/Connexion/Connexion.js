2b1172fc32e0eb606936948a42e614495292dc89
Cboudaud <camilleboudaud@gmail.com>
2023-02-24 10:40:39 +0100

    identation fixed

--- src/components/Connexion/Connexion.js
+++ src/components/Connexion/Connexion.js
@@ -1,12 +1,12 @@
 import './styles.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import { typeEmail, typePassword } from '../../actions/connexion';
-import { handleLogin } from '../../actions/authentication';
 import {
   Button,
   TextField,
-  FormControl
+  FormControl,
 } from '@mui/material';
+import { typeEmail, typePassword } from '../../actions/connexion';
+import { handleLogin } from '../../actions/authentication';
 
 export default function Connexion() {
   const { emailInput, passwordInput } = useSelector((state) => state.connexion);
@@ -19,33 +19,32 @@ export default function Connexion() {
 
   return (
     <div className="connexion">
-        <div className="connexion_title"> Connexion </div>
-          <form className="connexion_form" onSubmit={handleSubmit}>
-            <div className="connexion_form_item">
-              <FormControl fullWidth size="small">
-                <TextField 
-                    label="E-mail" 
-                    size="small" 
-                    value={emailInput} 
-                    onChange={(e) => dispatch(typeEmail(e.target.value))} 
-                />
-              </FormControl>
-            </div>
-            <div className="connexion_form_item">
-              <FormControl fullWidth size="small">
-                <TextField 
-                  label="Mot de passe" 
-                  size="small" 
-                  value={passwordInput} 
-                  onChange={(e) => dispatch(typePassword(e.target.value))} 
-                />
-              </FormControl>
-            </div>
-            <div className="connexion_login_button">
-              <Button variant="contained">Se connecter</Button>
-            </div>
-          </form>
-      </div>
-  )
-};
-
+      <div className="connexion_title"> Connexion </div>
+      <form className="connexion_form" onSubmit={handleSubmit}>
+        <div className="connexion_form_item">
+          <FormControl fullWidth size="small">
+            <TextField
+              label="E-mail"
+              size="small"
+              value={emailInput}
+              onChange={(e) => dispatch(typeEmail(e.target.value))}
+            />
+          </FormControl>
+        </div>
+        <div className="connexion_form_item">
+          <FormControl fullWidth size="small">
+            <TextField
+              label="Mot de passe"
+              size="small"
+              value={passwordInput}
+              onChange={(e) => dispatch(typePassword(e.target.value))}
+            />
+          </FormControl>
+        </div>
+        <div className="connexion_login_button">
+          <Button variant="contained">Se connecter</Button>
+        </div>
+      </form>
+    </div>
+  );
+}
