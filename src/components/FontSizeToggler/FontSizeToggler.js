69430d18a88784a40e8f25b8eebb3dd5d24fabb0
LucasPitzalis <lpitzalis@yahoo.fr>
2023-02-27 17:28:35 +0100

    fixed eslint formating

--- src/components/FontSizeToggler/FontSizeToggler.js
+++ src/components/FontSizeToggler/FontSizeToggler.js
@@ -1,34 +1,30 @@
 import './styles.scss';
-import * as React from 'react';
 import FormGroup from '@mui/material/FormGroup';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { FormControl } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeFontSize } from '../../actions/app';
-import {AntSwitch} from './AntSwitch';
-
+import AntSwitch from './AntSwitch';
 
 export default function FontSizeToggler() {
-
-
-const dispatch = useDispatch();
-const { largeFontSize } = useSelector((state) => state.app);
+  const dispatch = useDispatch();
+  const { largeFontSize } = useSelector((state) => state.app);
 
   return (
     <div className="fontsizetoggler">
       <FormControl component="fieldset">
         <FormGroup aria-label="position" row>
-          <Stack 
-            onChange={(e) => {
+          <Stack
+            onChange={() => {
               dispatch(changeFontSize());
-             }} 
-            direction="row" 
-            spacing={1} 
+            }}
+            direction="row"
+            spacing={1}
             alignItems="center"
           >
             <Typography>t</Typography>
-            <AntSwitch checked={ largeFontSize } />
+            <AntSwitch checked={largeFontSize} />
             <Typography>T</Typography>
           </Stack>
         </FormGroup>
