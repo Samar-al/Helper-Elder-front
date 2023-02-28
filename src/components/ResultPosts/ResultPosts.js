df1e64e64439c0fd23d65578054f09fe9ff7b454
Claire <trucy.claire92@gmail.com>
2023-02-28 16:42:39 +0100

    WIP Announcement and announcementList components removed

--- src/components/ResultPosts/ResultPosts.js
+++ src/components/ResultPosts/ResultPosts.js
@@ -1,11 +1,36 @@
 import './styles.scss';
+import PropTypes from 'prop-types';
+import { Pagination } from '@mui/material';
 import data from './data';
-import AnnouncementList from './AnnouncementList';
 
 export default function ResultPosts() {
   return (
     <div className="resultposts">
-      <AnnouncementList posts={data} />
+      <div className="list_posts">
+        <p className="list_posts_result">4 résultats sur 357</p>
+        <div className="posts">
+          {posts.map((post) => <ResultPosts key={post.id} {...post} />)}
+        </div>
+        <div className="pagination">
+          <Pagination count={10} variant="outlined" />
+        </div>
+      </div>
     </div>
   );
 }
+
+ResultPosts.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    picture: PropTypes.string.isRequired, // à modifier lorsqu'on saura comment récupérer l'avatar
+    title: PropTypes.string.isRequired,
+    zipcode: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    date: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.instanceOf(Date),
+    ]).isRequired,
+  })).isRequired,
+};
