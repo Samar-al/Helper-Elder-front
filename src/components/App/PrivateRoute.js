b87e0d15dd728a943e4a28acbc7ce9df6cc4685c
LucasPitzalis <lpitzalis@yahoo.fr>
2023-03-01 08:29:02 +0100

    comments

--- src/components/App/PrivateRoute.js
+++ src/components/App/PrivateRoute.js
@@ -2,6 +2,10 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+/* This component needs to be given as element prop
+for Routes that should be accessible to logged users only.
+If a user is present in the state (i.e. user is logged), returns element.
+If there is no user, returns a Navigate element to the connexion page */
 export default function PrivateRoute({ element }) {
   const { user } = useSelector((state) => state.authentication);
 
