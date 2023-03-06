1d0eb46732907903e87af2eb94b9f7fb6ce75df0
LucasPitzalis <lpitzalis@yahoo.fr>
2023-03-06 15:34:09 +0100

    review form validation OK

--- src/components/FormErrors/FormErrors.js
+++ src/components/FormErrors/FormErrors.js
@@ -1,8 +1,7 @@
 import './styles.scss';
+import PropTypes from 'prop-types';
 
-export default function FormErrors() {
-  const errors = ['le lavabo il est bo', 'le bidet il est laid'];
-
+export default function FormErrors({ errors }) {
   return (
     <div className="form_errors">
       <h3 className="form_errors_title">Echec lors de l'envoi du formulaire :</h3>
@@ -13,3 +12,7 @@ export default function FormErrors() {
     </div>
   );
 }
+
+FormErrors.propTypes = {
+  errors: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
