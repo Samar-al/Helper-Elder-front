1090ed77e5ea44881940ed59f74af438b2a7fee0
LucasPitzalis <113930943+LucasPitzalis@users.noreply.github.com>
2023-03-07 16:11:57 +0100

    Update Intro.js

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -6,7 +6,7 @@ export default function Intro() {
     <div className="intro">
       <h1 className="intro_title">HELPERS-ELDERS</h1>
       <p className="intro_content"> Bienvenue sur le site dédié à l'entraide entre particuliers et personnes âgées !
-        Vous cherchez de l'aide en tant qu'<span>Elder</span> ? Ou vous désirez proposer vos services d'<span>Helper</span> ? Alors vous êtes au bon endroit,
+        Vous cherchez de l'aide en tant qu'<strong>Elder</strong> ? Ou vous désirez proposer vos services d'<span>Helper</span> ? Alors vous êtes au bon endroit,
         grâce à notre système d'annonces et de messagerie vous trouverez sans aucun doute réponse à vos besoins !
       </p>
     </div>
